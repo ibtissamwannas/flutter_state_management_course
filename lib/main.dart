995fe13b66615ff241590a7995b08d9a7010f25e
ibtissam <ibtissamwannas21@gmail.com>
2025-11-23 08:59:34 +0200

    feat: remove ValueNotifier-based contact management example and simplify main app structure.

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:state_management_course/test_value_notifier.dart';
-import 'package:uuid/uuid.dart';
 
 void main() {
   runApp(const MyApp());
@@ -17,122 +15,16 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
         useMaterial3: false,
       ),
-      home: TextMain(),
-      routes: {'/new-contact': (context) => NewContact()},
+      home: HomePage(),
     );
   }
 }
 
-class Contact {
-  final String id;
-  final String name;
-  Contact({required this.name}) : id = const Uuid().v4();
-}
-
-class ContactBook extends ValueNotifier<List<Contact>> {
-  ContactBook._() : super([Contact(name: 'foo')]);
-  static final ContactBook _shared = ContactBook._();
-  factory ContactBook() => _shared;
-
-  int get length => value.length;
-
-  void add({required Contact contact}) {
-    final contacts = value;
-    contacts.add(contact);
-    notifyListeners();
-  }
-
-  void remove({required Contact contact}) {
-    final contacts = value;
-    if (contacts.contains(contact)) {
-      contacts.remove(contact);
-      notifyListeners();
-    }
-  }
-
-  Contact? contact({required int atIndex}) =>
-      value.length > atIndex ? value[atIndex] : null;
-}
-
-class MyHomePage extends StatelessWidget {
-  const MyHomePage({super.key});
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      floatingActionButton: FloatingActionButton(
-        backgroundColor: Colors.blue,
-        onPressed: () async {
-          await Navigator.of(context).pushNamed('/new-contact');
-        },
-        child: Icon(Icons.add),
-      ),
-      appBar: AppBar(backgroundColor: Colors.blue, title: Text("Home Page")),
-      body: ValueListenableBuilder(
-        valueListenable: ContactBook(),
-        builder: (BuildContext context, value, Widget? child) {
-          // final contacts = value;
-          return ListView.builder(
-            itemCount: value.length,
-            itemBuilder: (context, index) {
-              final contact = value[index];
-              return Dismissible(
-                onDismissed: (direction) {
-                  // contacts.remove(contact);
-                  ContactBook().remove(contact: contact);
-                },
-                key: ValueKey(contact.id),
-                child: ListTile(title: Text(contact.name)),
-              );
-            },
-          );
-        },
-      ),
-    );
-  }
-}
-
-class NewContact extends StatefulWidget {
-  const NewContact({super.key});
-
-  @override
-  State<NewContact> createState() => _NewContactState();
-}
-
-class _NewContactState extends State<NewContact> {
-  late final TextEditingController controller;
-
-  @override
-  void initState() {
-    controller = TextEditingController();
-    super.initState();
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.blue,
-        title: Text("Add a new Contact"),
-      ),
-      body: Column(
-        children: [
-          TextField(
-            controller: controller,
-            decoration: InputDecoration(
-              hintText: 'Enter a new contact name here',
-            ),
-          ),
-          TextButton(
-            onPressed: () {
-              final contact = Contact(name: controller.text);
-              ContactBook().add(contact: contact);
-              Navigator.of(context).pop();
-            },
-            child: const Text('Add Contact'),
-          ),
-        ],
-      ),
-    );
+    return const Placeholder();
   }
 }
